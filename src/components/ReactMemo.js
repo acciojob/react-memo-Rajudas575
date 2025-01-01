ce679ee6956fb9920559f7b4e91a4a0720c7069c
Rajudas575 <rajudas575@gmail.com>
2025-01-01 22:48:11 +0530

    Create ReactMemo.js

---
+++ src/components/ReactMemo.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const TodoList = React.memo(({ todos }) => {
+  console.log("Rendering TodoList");
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li key={index}>{todo}</li>
+      ))}
+    </ul>
+  );
+});
+
+const ReactMemoDemo = ({ todos }) => {
+  const [newTodo, setNewTodo] = useState("");
+  const [customTodos, setCustomTodos] = useState(todos);
+
+  const addCustomTodo = () => {
+    if (newTodo.length > 5) {
+      setCustomTodos([...customTodos, newTodo]);
+      setNewTodo("");
+    } else {
+      alert("Task must be more than 5 characters long.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>React.memo</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a task"
+      />
+      <button onClick={addCustomTodo}>Add Skill</button>
+      <TodoList todos={customTodos} />
+    </div>
+  );
+};
+
+export default ReactMemoDemo;
