e0cbf710f2c3be0f1cdd4fc8ca2f34ee80e5dbde
Rajudas575 <rajudas575@gmail.com>
2025-01-03 11:16:12 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -1,44 +1,17 @@
-import React, { useState } from "react";
+import React from 'react';
 
-const TodoList = React.memo(({ todos }) => {
-  console.log("Rendering TodoList");
-  return (
-    <ul>
-      {todos.map((todo, index) => (
-        <li key={index}>{todo}</li>
-      ))}
-    </ul>
-  );
-});
-
-const ReactMemo = ({ todos, setTodos }) => {
-  const [newTodo, setNewTodo] = useState("");
-
-  const addCustomTodo = () => {
-    if (newTodo.length > 5) {
-      setTodos([...todos, newTodo]);
-      setNewTodo("");
-    } else {
-      alert("Task must be more than 5 characters long.");
-    }
-  };
-
+const ReactMemo = React.memo(({ todos }) => {
+  console.log('ReactMemo Render');
   return (
     <div>
-      <h2>Add Custom Task</h2>
-      <input
-        id="add-skill-input"
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Add a task"
-      />
-      <button id="add-skill-button" onClick={addCustomTodo}>
-        Add Skill
-      </button>
-      <TodoList todos={todos} />
+      <h3>Filtered Todos</h3>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
-};
+});
 
 export default ReactMemo;
